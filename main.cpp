bd67981a89cc704ea8d3c39efc95a2d373d302bf
Tom <89728113+tomm02@users.noreply.github.com>
2021-09-01 17:51:56 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+// Include pcio libraries
+#include "pico/stdlib.h"
+// Include stdio or iostream if you want to print via serial
+#include <stdio.h>
+
+#define LED_BUILTIN 25
+
+int main() {
+	// initialise the built in led
+	gpio_init(LED_BUILTIN);
+	gpio_set_dir(LED_BUILTIN, GPIO_OUT);
+	// initialise stdio to allow printing
+	stdio_init_all();
+
+	while (true) {
+		// set the led to high
+		gpio_put(LED_BUILTIN, 1);
+		// sleep 500 ms
+		sleep_ms(500);
+		// print a string
+		printf("Hello!\n");
+		// set the led to low
+		gpio_put(LED_BUILTIN, 0);
+		// sleep 500 ms
+		sleep_ms(500);
+	}
+}
